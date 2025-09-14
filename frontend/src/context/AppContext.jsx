6ea687ebea32f961e984deded32b90ca87a080c8
Kujins <sivaneswarankujinsika@gmail.com>
2025-09-14 23:23:12 +0530

    all products part completed

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -17,6 +17,7 @@ export const AppContextProvider = ({children}) =>{
     const [products,setProducts] = useState([]);
 
     const [cartItems,setCartItems] = useState({});
+    const [searchQuery,setSearchQuery] = useState({});
 
     const fetchProducts = async ()=>{
         setProducts(dummyProducts)
@@ -57,7 +58,7 @@ export const AppContextProvider = ({children}) =>{
         fetchProducts()
     },[])
 
-    const value ={navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin,products,currency,updateCartItem,removeFromCart,cartItems,addToCart}
+    const value ={navigate,user,setUser,isSeller,setIsSeller,showUserLogin,setShowUserLogin,products,currency,updateCartItem,removeFromCart,cartItems,addToCart,searchQuery,setSearchQuery}
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
